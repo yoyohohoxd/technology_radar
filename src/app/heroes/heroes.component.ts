36e2b5fb405a65a77bb88ded86814ece7bf9b1d7
DMHN (Mads-Emil Hansen) <DMHN@novonordisk.com>
2021-07-13 13:38:45 +0200

    Made hero component

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,13 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.scss']
 })
+
 export class HeroesComponent implements OnInit {
 
   constructor() { }
+
+  hero: Hero = {
+    id: 1,
+    name: 'Mads-Emil Kjeldsen Hansen'
+  };
 
   ngOnInit(): void {
   }
