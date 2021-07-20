15ae107dd5a3f7a2b8ce8be28adb6215fccdc3b2
DMHN (Mads-Emil Hansen) <DMHN@novonordisk.com>
2021-07-20 14:35:01 +0200

    add messages-component and messages-service. the component is injected into html via app.component template and the service is injected into parent component.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 
 @Component({
@@ -11,23 +13,20 @@ import { HeroService } from '../hero.service';
 
 export class HeroesComponent implements OnInit {
 
-  constructor(private heroService: HeroService) { }
+  heroes: Hero[] = [];
+
+  selectedHero?: Hero;
+
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
 
   getHeroes(): void {
     this.heroService.getHeroes()
     .subscribe(heroes => this.heroes = heroes);
   }
 
-  hero: Hero = {
-    id: 1,
-    name: 'Mads-Emil Kjeldsen Hansen'
-  };
-
-  heroes: Hero[] = [];
-
-  selectedHero?: Hero;
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=$(hero.id)`)
   }
 
   ngOnInit(): void {
