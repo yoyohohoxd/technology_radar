f44c32a8c945e2939afe74bc1f61d23ed191182b
DMHN (Mads-Emil Hansen) <DMHN@novonordisk.com>
2021-07-20 16:11:03 +0200

    add routing and cleaned up. https://angular.io/tutorial/toh-pt5#dashboardcomponent-hero-links

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -12,25 +12,16 @@ import { MessageService } from '../message.service';
 })
 
 export class HeroesComponent implements OnInit {
-
   heroes: Hero[] = [];
 
-  selectedHero?: Hero;
+  constructor(private heroService: HeroService) { }
 
-  constructor(private heroService: HeroService, private messageService: MessageService) { }
+  ngOnInit() {
+    this.getHeroes();
+  }
 
   getHeroes(): void {
     this.heroService.getHeroes()
     .subscribe(heroes => this.heroes = heroes);
   }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=$(hero.id)`)
-  }
-
-  ngOnInit(): void {
-    this.getHeroes();
-  }
-
 }
