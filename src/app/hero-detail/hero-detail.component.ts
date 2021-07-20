82f6ce509868d63aaa51b56638b000a45fdfe96d
DMHN (Mads-Emil Hansen) <DMHN@novonordisk.com>
2021-07-20 10:32:58 +0200

    made hero-detail-component work. can now be deligated to other components.

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Hero } from '../hero';
 
 @Component({
   selector: 'app-hero-detail',
@@ -8,6 +9,8 @@ import { Component, OnInit } from '@angular/core';
 export class HeroDetailComponent implements OnInit {
 
   constructor() { }
+
+  @Input() hero?: Hero;
 
   ngOnInit(): void {
   }
