bb85299d1a2499b6d5c7b45f329f3f6282578c3d
DMHN (Mads-Emil Hansen) <DMHN@novonordisk.com>
2021-07-20 13:59:13 +0200

    made the first hero-service work to make the data-fetching asynchronous. generated a new message component

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+
 
 @Component({
   selector: 'app-heroes',
@@ -10,14 +11,19 @@ import { HEROES } from '../mock-heroes';
 
 export class HeroesComponent implements OnInit {
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+    .subscribe(heroes => this.heroes = heroes);
+  }
 
   hero: Hero = {
     id: 1,
     name: 'Mads-Emil Kjeldsen Hansen'
   };
 
-  heroes = HEROES;
+  heroes: Hero[] = [];
 
   selectedHero?: Hero;
   onSelect(hero: Hero): void {
@@ -25,6 +31,7 @@ export class HeroesComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.getHeroes();
   }
 
 }
