7ad2ebf29e7169bb0be3f6bfeb6d770b4349f4aa
Mads-Emil Kjeldsen Hansen <madse@live.dk>
2021-07-20 22:56:25 +0200

    commit from personal computer. done with routing.

--- src/app/message.service.ts
+++ src/app/message.service.ts
@@ -5,14 +5,20 @@ import { Injectable } from '@angular/core';
 })
 export class MessageService {
 
+  messageNumber = 0;
+
   messages: string[] = [];
 
   add(message: string) {
+    this.messageNumber += 1;
+    var messageNumberString = this.messageNumber.toString();
+    message = messageNumberString + " " + message;
     this.messages.push(message);
   }
 
   clear() {
     this.messages = [];
+    this.messageNumber = 0;
   }
   
 }
